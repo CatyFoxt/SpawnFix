a0f3de00289d047d7a3a733a27a7f83bf87561dc
Caty Foxt <124933902+CatyFoxt@users.noreply.github.com>
2024-05-18 16:23:54 +0200

    Tps Command

---
+++ RespawnFix/tps.cs
@@ -0,0 +1,22 @@
+﻿using CommandSystem;
+using System;
+using Exiled.API.Features;
+using RespawnFix;
+
+namespace FoxtUtils.Commands
+{
+    [CommandHandler(typeof(ClientCommandHandler))]
+    [CommandHandler(typeof(RemoteAdminCommandHandler))]
+    public class TpsCommand : ICommand
+    {
+        public string Command { get; } = "tps";
+        public string[] Aliases { get; } = { };
+        public string Description { get; } = " Check the tps.";
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            response = "Tps: " + Server.Tps;
+            return true;
+        }
+    }
+}
+
